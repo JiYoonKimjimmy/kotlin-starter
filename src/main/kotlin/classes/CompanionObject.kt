1a3aed872766a9f7d8a614849be65af2495d3ce2
jimmyberg <jimmyberg.kim@gmail.com>
2022-03-22 00:14:28 +0900

    add anonymous object

--- src/main/kotlin/classes/CompanionObject.kt
+++ src/main/kotlin/classes/CompanionObject.kt
@@ -1,9 +1,22 @@
 package classes
 
+import java.awt.Window
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
+
 class User3 private constructor(val nickName: String) {
     companion object {
         fun newNickName(email: String) = User3(email.substringBefore('@'))
     }
+}
+
+fun countClicks(window: Window) {
+    var clickCount = 0
+    window.addMouseListener(object : MouseAdapter() {
+        override fun mouseClicked(e: MouseEvent) {
+            clickCount++
+        }
+    })
 }
 
 fun main() {
