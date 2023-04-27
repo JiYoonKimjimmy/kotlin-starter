50f990dceebc30d5ecd5a1e6de1bc0cd6e2b05a0
jimmyberg <jimmyberg.kim@gmail.com>
2023-04-27 18:55:12 +0900

    RxKotlinExample.kt 수정
    - RxKotlinExample 클래스 정리
    - main 함수 RxKotlinExample 클래스 호출 처리

--- src/main/kotlin/rxkotlin/RxKotlinExample.kt
+++ src/main/kotlin/rxkotlin/RxKotlinExample.kt
@@ -7,32 +7,32 @@ import io.reactivex.subjects.PublishSubject
 import io.reactivex.subjects.Subject
 
 class RxKotlinExample {
+    fun run() {
+        println("Hello RxKotlin")
+        val list:List<Any> = listOf("One", "Two", "Three")
+        val observable: Observable<Any> = list.toObservable()
 
+        observable.subscribeBy(
+            onNext = { println(it) },
+            onError = { it.printStackTrace() },
+            onComplete = { println("DONE!!") }
+        )
+
+        val subject: Subject<Int> = PublishSubject.create()
+
+        subject
+            .map { isEven(it) }
+            .subscribe {
+                println("The number is ${(if (it) "Even" else "Odd")}")
+            }
+
+        subject.onNext(2)
+        subject.onNext(3)
+    }
+
+    private fun isEven(n: Int): Boolean = (n % 2 == 0)
 }
 
 fun main() {
-    println("Hello RxKotlin")
-    val list:List<Any> = listOf("One", "Two", "Three")
-    val obsevable: Observable<Any> = list.toObservable()
-
-    obsevable.subscribeBy(
-        onNext = { println(it) },
-        onError = { it.printStackTrace() },
-        onComplete = { println("DONE!!") }
-    )
-
-    val subject: Subject<Int> = PublishSubject.create()
-
-    subject
-        .map { isEven(it) }
-        .subscribe {
-            println("The number is ${(if (it) "Even" else "Odd")}")
-        }
-
-    subject.onNext(2)
-    subject.onNext(3)
-
-
+    RxKotlinExample().run()
 }
-
-fun isEven(n: Int): Boolean = (n % 2 == 0)
